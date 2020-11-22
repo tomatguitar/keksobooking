92d108d5d713000980d422119d05608ba96843a5
Dmytro Zhyltsov <zkarman@mail.ru>
2020-11-23 00:39:49 +0200

    refactor: change vars to const and lets

--- js/map.js
+++ js/map.js
@@ -1,47 +1,47 @@
 'use strict';
 
-var places = [];
-var titles = ["Большая уютная квартира", "Маленькая неуютная квартира", "Оргомный прекрасный дворец", 
+let places = [];
+const titles = ["Большая уютная квартира", "Маленькая неуютная квартира", "Оргомный прекрасный дворец", 
                "Маленький ужасный дворец", "Красивый гостевой домик", "Некрасивый негостеприимный домик", 
                "Ужасное бунгало по колено в воде", "Уютное бунгало далеко от моря"];
-var types = ["palace", "flat", "house", "bungalo"];
-var checkIns = ["12:00", "13:00", "14:00"];
-var checkOuts = ["12:00", "13:00", "14:00"];
-var features = ["wifi", "dishwaher", "parking", "washer", "elevator", "conditioner"];
-var photos = ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", 
+const types = ["palace", "flat", "house", "bungalo"];
+const checkIns = ["12:00", "13:00", "14:00"];
+const checkOuts = ["12:00", "13:00", "14:00"];
+const features = ["wifi", "dishwaher", "parking", "washer", "elevator", "conditioner"];
+const photos = ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", 
               "http://o0.github.io/assets/images/tokyo/hotel2.jpg",
               "http://o0.github.io/assets/images/tokyo/hotel3.jpg"];
            
 function generatePlace (place, title, checkIn, checkOut, types, feature, photo) {
-    var minX = 130;
-    var maxX = 630;
+    const minX = 130;
+    const maxX = 630;
     
-    var minY = 130;
-    var maxY = 630;
+    const minY = 130;
+    const maxY = 630;
     
-    var minPrice = 1000;
-    var maxPrice = 1000000;
+    const minPrice = 1000;
+    const maxPrice = 1000000;
     
-    var minRoom = 1;
-    var maxRoom = 5;
+    const minRoom = 1;
+    const maxRoom = 5;
     
-    var minGuests = 1;
-    var maxGuests = 10;
+    const minGuests = 1;
+    const maxGuests = 10;
     
-    var x =  generateRandVal(minX, maxX);
-    var y =  generateRandVal(minY, maxY);
+    const x =  generateRandVal(minX, maxX);
+    const y =  generateRandVal(minY, maxY);
     
     function generateRandArrLen (arr) {
-        var result = [];
-        var randLen = Math.floor(Math.random()*arr.length);
-        for (var i=0; i<=randLen; i++){
+        let result = [];
+        let randLen = Math.floor(Math.random()*arr.length);
+        for (let i=0; i<=randLen; i++){
             result.push(arr[i]);
         } 
         return result;
     }
     
     function generateRandArrElem (arr) {
-        var rand = Math.floor(Math.random()*arr.length);
+        let rand = Math.floor(Math.random()*arr.length);
         return arr[rand];
     };
     
@@ -49,8 +49,8 @@ function generatePlace (place, title, checkIn, checkOut, types, feature, photo) 
             return  Math.floor(minVal + Math.random() * (maxVal + 1 - minVal));  
         };
     
-    for(var i=0; i <= title.length; i++){
-        var j = i + 1;
+    for(let i=0; i <= title.length; i++){
+        let j = i + 1;
         place.push({
                 "author": "img/avatar/user0" + j +".png",
                 "offer": {
@@ -79,5 +79,5 @@ generatePlace(places, titles, checkIns, checkOuts, types, features, photos);
 
 document.querySelector('.map').classList.remove('map--faded');
 
-var fragment = document.createDocumentFragment('template').content.
+let fragment = document.createDocumentFragment('template').content.
 
